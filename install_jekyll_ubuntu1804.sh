4fcd6429e95062848f34ef87d1a1ab30cc24f912
Manuel Fedele <manuelfedele@gmail.com>
2020-01-14 18:27:18 +0100

    First real commit

---
+++ install_jekyll_ubuntu1804.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y ruby-full build-essential zlib1g-dev
+
+echo '# Install Ruby Gems to ~/gems' >> ~/.zshrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.zshrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.zshrc
+source ~/.zshrc
+
+gem install jekyll bundler
